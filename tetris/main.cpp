7269a8b904938ee2d05b62ef9188bd8d2e397365
vesbeast1703 <119749554+vesbeast1703@users.noreply.github.com>
2024-06-11 18:40:13 +0300

    Setup the game loop
    Пошло-поехало

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
+#include <raylib.h>
 
 int main() {
-	std::cout << "Hello, World!\n";
+	Color darkBlue = { 50, 50, 100, 200 };  // цвет фона
+	InitWindow(300, 600, "SEVA-Tetris");  // размеры окна и название шапки
+	SetTargetFPS(60);  
+	while (!WindowShouldClose()) 
+	{
+		BeginDrawing();
+		ClearBackground(darkBlue);
+		EndDrawing();
+	}
+	CloseWindow();
 }
